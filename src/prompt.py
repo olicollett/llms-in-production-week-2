ca0e01d550995945460f9f404c67cac7b5979fab
Oliver Collett <oliver.collett@sony.com>
2024-11-14 14:45:27 +0000

    updated prompt and temperature

--- src/prompt.py
+++ src/prompt.py
@@ -1,7 +1,8 @@
 PROMPT = """
-Generate a valid SQL query for the following natural language instruction:
-
-Query: ${query}
+You will be given a prompt which you will have to translate to SQL.
+Here is the SQL Query: ${query}
+Make sure you only ouput sql code and no ```sql```.
 
 ${gr.complete_json_suffix_v3}
+
 """
