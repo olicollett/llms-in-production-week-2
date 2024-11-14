296bc746a2cf118b835c955bb636dab20782a329
Oliver Collett <oliver.collett@sony.com>
2024-11-14 15:49:53 +0000

    added redis cache

--- src/cached_resources.py
+++ src/cached_resources.py
@@ -1,10 +1,12 @@
 import guardrails as gd
 import streamlit as st
+from redisvl.extensions.llmcache import SemanticCache 
 from phoenix.otel import register
 from openinference.instrumentation.openai import OpenAIInstrumentor
 
 from src.models import LLMResponse
 from src.prompt import PROMPT
+import redis
 
 
 @st.cache_resource
@@ -23,3 +25,23 @@ def instrument() -> None:
         endpoint="http://phoenix:6006/v1/traces",
     )
     OpenAIInstrumentor().instrument(tracer_provider=tracer_provider)
+
+@st.cache_resource
+def get_semantic_cache() -> SemanticCache:
+    """
+    Create a cache using RedisVL SemanticCache.
+    """
+    llmcache = SemanticCache(
+        name="llmcache",                    
+        prefix="llmcache",                   
+        redis_url="redis://redis:6379",      
+        distance_threshold=0.1               
+    )
+    return llmcache
+
+@st.cache_resource
+def get_exact_match_cache() -> redis.Redis:
+    """
+    Create a Redis connection for exact match cache using Redis Hash Map.
+    """
+    return redis.Redis(host='redis', port=6379, db=0)
